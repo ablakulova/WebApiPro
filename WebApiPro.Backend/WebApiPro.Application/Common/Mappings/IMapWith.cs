542cd5bece29b70b1891218f746a89cdcae6ab1d
ablakulova <mablaqulova@gmail.com>
2021-11-21 12:47:03 +0500

    3.AutoMapper added

---
+++ WebApiPro.Backend/WebApiPro.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,10 @@
+﻿using AutoMapper;
+
+namespace WebApiPro.Application.Common.Mappings
+{
+    public interface IMapWith<T>
+    {
+        void Mapping(Profile profile) =>
+            profile.CreateMap(typeof(T), GetType());
+    }
+}
