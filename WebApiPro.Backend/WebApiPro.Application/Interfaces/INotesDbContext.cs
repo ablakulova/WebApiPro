7f3d455284bd9c744bedb12e106aabf8248e723b
ablakulova <mablaqulova@gmail.com>
2021-11-21 12:06:12 +0500

    2.Persistence modified

---
+++ WebApiPro.Backend/WebApiPro.Application/Interfaces/INotesDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+using WebApiPro.Domain;
+
+namespace WebApiPro.Application.Interfaces
+{
+    public interface INotesDbContext
+    {
+        DbSet<Note> Notes { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ WebApiPro.Backend/WebApiPro.Persistence/NotesDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiPro.Application.Interfaces;
+using WebApiPro.Domain;
+using WebApiPro.Persistence.EntityTypeConfigurations;
+
+namespace WebApiPro.Persistence
+{
+    public class NotesDbContext : DbContext, INotesDbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+
+        public NotesDbContext(DbContextOptions<NotesDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new NoteConfiguration());
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
